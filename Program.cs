3c4dfbd6b5552d00ed214ea4af8b42fb53ec6526
Blake Bybee <Blakebybeee@gmail.com>
2022-11-03 16:59:58 -0600

    finished commit

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿Console.WriteLine("HELLO THERE!!!");
+Console.WriteLine("DO YOU KNOW HOW TO PLAY TEXAS HOLD'EM? yes or no");
+bool user=true;
+if (user == true)
+{
+string answer = (Console.ReadLine ());
+ if (answer== "yes")
+    {
+        Console.WriteLine ("GET ONTO THE GAME AND DON'T LOSE ALL YOUR MONEY ;)");
+    }
+
+ if (answer == "no")
+ {
+    Console.WriteLine ("In Texas hold'em each player is dealt two cards called their 'hole' cards.");
+    Console.WriteLine ("Hole cards can only be seen and used by one person.");
+    Console.WriteLine ("The dealer button is located before hands are dealt to allow for the positioning of the forced bets.");
+    Console.WriteLine("Small blind and big blind, and also to determine who will act first and last in the hand");
+    Console.WriteLine();
+    Console.WriteLine("The blinds are an important part of the rules of poker.");
+    Console.WriteLine("These forced bets which give players an incentive to play; in other words 'spice up' the game.");
+    Console.WriteLine("Without the blinds, there would be no penalty for waiting and only playing strong hands.");
+    Console.WriteLine("The only hand worth playing would be two aces!");
+    Console.WriteLine("");
+    Console.WriteLine("PRE-FLOP= The First Betting Round of Hold'em");
+    Console.WriteLine("The first round of betting takes places starting at the position to the left of the big blind");
+    Console.WriteLine("");
+    Console.WriteLine("Each player has the following options:");
+    Console.WriteLine("");
+    Console.WriteLine("RAISE");
+    Console.WriteLine("");
+    Console.WriteLine("you can raise the current bet to increase the stakes of the game.");
+    Console.WriteLine("If someone has raised before you, you can still raise again, this is call a reraise.");
+    Console.WriteLine("");
+    Console.WriteLine("CALL");
+    Console.WriteLine("");
+    Console.WriteLine("When you do not want to raise the stakes but want to continue with your hand you can match the current bet.");
+    Console.WriteLine("");
+    Console.WriteLine("FOLD");
+    Console.WriteLine("");
+    Console.WriteLine("If you feel your hand is not worth playing any further you can fold your hand and not commit any more bets.");
+    Console.WriteLine("");
+    Console.WriteLine("CHECK");
+    Console.WriteLine("");
+    Console.WriteLine("If there is no bet placed you can check in order to see the next card. This isn't applicable to preflop.");
+    Console.WriteLine("The blinds are the first bet preflop which must be matched with a call or raised, if a player wishes to continue.");
+    Console.WriteLine("Players must act in sequence until all bets are settled. The button must always act last in the first sequence.");
+    Console.WriteLine("");
+    Console.WriteLine("The Flop - The Second Betting Round");
+    Console.WriteLine("");
+    Console.WriteLine("The second round of betting takes places after the three community (shared) cards called the flop are dealt.");
+    Console.WriteLine("The action will be to the first player to the left of the dealer. ");
+    Console.WriteLine("The first player to act has the option to check bet or fold; although you should not fold when you can check for free.");
+    Console.WriteLine("");
+    Console.WriteLine("The Turn - The Third Betting Round");
+    Console.WriteLine("");
+    Console.WriteLine("The third round of betting occurs after the second community card has been dealt.");
+    Console.WriteLine("This card is called the turn. Again, the action starts with the active player to the left of the dealer.");
+    Console.WriteLine("");
+    Console.WriteLine("The River and Showdown - The Fourth Betting Round");
+    Console.WriteLine("");
+    Console.WriteLine("The fourth and final round of betting occurs when the dealer turns over the river card. ");
+    Console.WriteLine("The hand ends with the showdown of hands or if there is only one live hand remaining (the other player(s) have folded).");
+    Console.WriteLine("");
+    Console.WriteLine("At showdown, the player with the best five card combination from their hole cards and the community cards wins the final pot.");
+    Console.WriteLine("Split pots occur when both players have the same best five cards.");
+    Console.WriteLine("");
+    Console.WriteLine("After each hand, the button moves to the left of the dealer. This means everyone will have to play the blinds at some point.");
+    Console.WriteLine("");
+    Console.WriteLine("Do you know the Poker Hand Ranking System ");
+    string answer2 = (Console.ReadLine ());
+    if (answer2== "yes")
+    {
+        Console.WriteLine ("GET ONTO THE GAME AND DON'T LOSE ALL YOUR MONEY ;)");
+    }
+    if (answer2 == "no")
+ {
+    Console.WriteLine("These are in order from best to worst hands");
+    Console.WriteLine("");
+    Console.WriteLine("Royal Flush - Ace, King, Queen, Jack, 10 of all the same suit");
+    Console.WriteLine("");
+    Console.WriteLine("Straight Flush- Any straight that is the same suit");
+    Console.WriteLine("");
+    Console.WriteLine("Four of a Kind - Any 4 cards with the same number (four 7's or four 9's) ");
+    Console.WriteLine("");
+    Console.WriteLine("Full House - 3 of a kind, and pair - example (three 4's and two jacks");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+}
+}
+}
+
